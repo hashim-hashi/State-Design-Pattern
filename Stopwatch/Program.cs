cfcc8f55cddcc068afdc4f571af7006a2835ed77
Hashim <hashimhashi010@gmail.com>
2022-12-07 11:15:10 +0530

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StopWatch
+{
+    internal interface IContextBase
+    {
+        void PressFirstButton();
+        void PressSecondButton();
+
+    }
+    internal interface IState
+    {
+        String TextOnFirstButton();
+        String TextOnSecondButton();
+        IState PressFirstButton();
+        IState PressSecondButton();
+    }
+
+    namespace ConcreteState
+    {
+        // Actions When Clicks on Buttons at InitialScreenState
+        internal class InitialScreenState : IState
+        {
+            public IState PressFirstButton()
+            {
+                Console.WriteLine("StopWatch Started");
+                return new RunningState();
+            }
+            public IState PressSecondButton()
+            {
+                return new InitialScreenState();
+            }
+
+            public string TextOnFirstButton()
+            {
+                return "Start";
+            }
+
+            public string TextOnSecondButton()
+            {
+                return "Lap";
+            }
+        }
+
+        // Actions When Clicks on Buttons at LappingState
+
+        internal class LappingState : IState
+        {
+            public IState PressFirstButton()
+            {
+                Console.WriteLine("StopWatch Stoped");
+                return new DisplayingResultState();
+            }
+
+            public IState PressSecondButton()
+            {
+                Console.WriteLine("StopWatch Lap");
+                return new LappingState();
+            }
+
+            public string TextOnFirstButton()
+            {
+                return "Stop";
+            }
+
+            public string TextOnSecondButton()
+            {
+                return "Lap";
+            }
+        }
+
+        // Actions When Clicks on Buttons at RunningState
+        internal class RunningState : IState
+        {
+            public IState PressFirstButton()
+            {
+                Console.WriteLine("StopWatch Started");
+                return new InitialScreenState();
+            }
+
+            public IState PressSecondButton()
+            {
+                Console.WriteLine("StopWatch Lap");
+                return new LappingState();
+            }
+
+            public string TextOnFirstButton()
+            {
+                return "Stop";
+            }
+
+            public string TextOnSecondButton()
+            {
+                return "Lap";
+            }
+        }
+
+        // Actions When Clicks on Buttons at DisplayingResultState
+        internal class DisplayingResultState : IState
+        {
+            public IState PressFirstButton()
+            {
+                Console.WriteLine("StopWatch ReStarted");
+                return new RunningState();
+            }
+
+            public IState PressSecondButton()
+            {
+                Console.WriteLine("StopWatch Reset");
+                return new InitialScreenState();
+            }
+
+            public string TextOnFirstButton()
+            {
+                return "Start";
+            }
+
+            public string TextOnSecondButton()
+            {
+                return "Reset";
+            }
+        }
+    }
+
+    internal class Context : IContextBase
+    {
+        public Context(IState state)
+        {
+            State = state;
+        }
+        public IState State { get; set; }
+        private void DisplayText()
+        {
+            Console.WriteLine("Current State[{0}], Button1[{1}], Button2[{2}]", State.GetType().Name, State.TextOnFirstButton(), State.TextOnSecondButton());
+        }
+        public void PressFirstButton()
+        {
+            State = State.PressFirstButton();
+            DisplayText();
+        }
+        public void PressSecondButton()
+        {
+            State = State.PressSecondButton();
+            DisplayText();
+        }
+    }
+}
---
+++ Stopwatch/StopWatch.cs
@@ -0,0 +1,14 @@
+﻿StopWatch.IContextBase stopwatch = new StopWatch.Context(new StopWatch.ConcreteState.InitialScreenState());
+
+//press 2nd button => nothing happens
+stopwatch.PressSecondButton();//nothing happen
+
+//start, lap, lap, stop, restart, lap, stop, reset
+stopwatch.PressFirstButton();//started
+stopwatch.PressSecondButton();//lap
+stopwatch.PressSecondButton();//lap
+stopwatch.PressFirstButton();//stop
+stopwatch.PressFirstButton();//restart
+stopwatch.PressSecondButton();//lap
+stopwatch.PressFirstButton();//stop
+stopwatch.PressSecondButton();//reset
